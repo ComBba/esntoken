d31cd516cf779474d6ea625a6a58716220d454a0
ComBba <cent@naver.com>
2024-09-25 21:16:10 +0900

    표준 토큰

--- contracts/esnToken.sol
+++ contracts/esnToken.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ESNToken is ERC20 {
-    constructor() ERC20("ESN Token", "ESN") {
-        uint256 initialSupply = 100_000_000 * 10 ** decimals();
+    constructor(uint256 initialSupply) ERC20("ESN Token", "ESN") {
         _mint(msg.sender, initialSupply);
     }
 }
